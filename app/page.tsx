1f8ccdf01965ffc3e14ddc357da98314d209273e
Joyful <mail@lidaze.com>
2024-07-26 11:54:28 +0800

    [feat] responsive design

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import clsx from "clsx";
 import PersonalInfo from "@/components/personal-info";
 import Projects from "@/components/projects";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen max-w-screen-lg mx-auto justify-between">
-      <PersonalInfo className="w-[360px] p-10" />
+    <main className={clsx("min-h-screen max-w-screen-lg mx-auto", "flex flex-col gap-4 items-center sm:flex-row sm:items-start")}>
+      <PersonalInfo className="w-full sm:w-[360px] p-10" />
       <Projects className="flex-1 p-10" />
     </main>
   );
