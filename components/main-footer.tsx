0d0423d9db6b336058f83f6f825ae9369fdae230
Joyful <mail@lidaze.com>
2024-07-29 11:00:38 +0800

    [feat] add copyright

--- components/main-footer.tsx
+++ components/main-footer.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import ThemeToggle from "./theme-toggle";
 import LanguageToggle from "./language-toggle";
+import { Heart } from "lucide-react";
 
 interface Props {
   className?: string;
@@ -8,11 +9,16 @@ interface Props {
 
 export default function MainFooter({ className }: Props) {
   return (
-    <footer className={clsx("mt-20 py-4", className)}>
+    <footer className={clsx("mt-20 pt-4", className)}>
       <div className="flex items-center justify-end gap-x-6">
         <LanguageToggle />
         <ThemeToggle />
       </div>
+      <p className="text-muted-foreground text-xs flex items-center justify-center gap-x-2 p-3">
+        <Heart className="size-3 opacity-60" />
+        <span>Made by Joyful out of passion 2024-07</span>
+        <Heart className="size-3 opacity-60" />
+      </p>
     </footer>
   );
 }
