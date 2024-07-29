81d456e27fd965567f6d31497b14f94e8278dbe4
Joyful <mail@lidaze.com>
2024-07-29 11:26:48 +0800

    [feat] style polish

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -5,8 +5,8 @@ import MainFooter from "@/components/main-footer";
 export default function Home() {
   return (
     <main className="min-h-screen max-w-screen-lg mx-auto pt-8 px-8">
-      <div className="flex flex-col gap-4 items-center sm:flex-row sm:items-start">
-        <PersonalInfo className="w-full sm:w-[360px]" />
+      <div className="flex flex-col gap-8 items-center sm:flex-row sm:items-start">
+        <PersonalInfo className="w-full sm:w-[320px] sm:sticky sm:top-4" />
         <Projects className="flex-1 sm:mt-0" />
       </div>
       <MainFooter />
--- components/main-footer.tsx
+++ components/main-footer.tsx
@@ -9,12 +9,12 @@ interface Props {
 
 export default function MainFooter({ className }: Props) {
   return (
-    <footer className={clsx("mt-20 pt-4", className)}>
+    <footer className={clsx("mt-6 sm:mt-12 pt-4", className)}>
       <div className="flex items-center justify-end gap-x-6">
         <LanguageToggle />
         <ThemeToggle />
       </div>
-      <p className="text-muted-foreground text-xs flex items-center justify-center gap-x-2 p-3">
+      <p className="text-muted-foreground text-xs flex items-center justify-center gap-x-2 p-4">
         <Heart className="size-3 opacity-60" />
         <span>Made by Joyful out of passion 2024-07</span>
         <Heart className="size-3 opacity-60" />
